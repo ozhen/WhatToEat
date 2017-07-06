d8017e68541fad94d0b31d3f106d7f070e619a8e
ozhen <ozhen@sfu.ca>
2017-07-06 14:12:10 -0700

    Restaurant names are listed after submitting city and county

--- WhatToEat-revision1/app/controllers/pages_controller.rb
+++ WhatToEat-revision1/app/controllers/pages_controller.rb
@@ -9,6 +9,7 @@ class PagesController < ApplicationController
   	city = params[:page][:city].downcase
   	country = params[:page][:country].downcase
 
+    #Google Maps API url using city and country
   	base_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+city+","+country
 
   	response = RestClient.get base_url+city+","+country
@@ -18,6 +19,7 @@ class PagesController < ApplicationController
   	lat = data['results'][0]['geometry']['location']['lat']
   	lng = data['results'][0]['geometry']['location']['lng']
 
+    #Google Maps API nearbysearch url
   	base_url2 = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
 
   	response2 = RestClient.get base_url2+"#{lat}"+","+"#{lng}"+"&radius=500&type=restaurant&key=AIzaSyDA_-IX_8NMtw7Mu5iNTucMDuUw4vFtPn4"
@@ -26,7 +28,20 @@ class PagesController < ApplicationController
 
   	restaurant1 = data2
 
-  	flash.now[:danger] = restaurant1
+    #create a list that stores restaurant names
+    restaurant_list = Array.new
+
+    #put restaurantt names in the list
+    for i in 0..19
+      restaurant_list.push(data2['results'][i]['name'])
+    end
+  
+
+
+
+    #display restaurant names
+  	#flash.now[:danger] = restaurant1
+    flash.now[:danger] = restaurant_list
   	render 'weather'
   end
 end
